b30fac695123181daff80499fbf33ee1f4e313ab
Alfonso Rodriguez <ponchin24@gmail.com>
2021-10-16 20:37:13 -0400

    Adding queries and retirement data files.

--- schema.sql
+++ schema.sql
@@ -17,22 +17,22 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-    emp_no INT NOT NULL,
-    salary INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -49,9 +49,8 @@ CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
 );
 
-SELECT * FROM departments;
+
+SELECT * FROM titles;
